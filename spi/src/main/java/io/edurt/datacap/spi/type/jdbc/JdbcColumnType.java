f3ecb66e2921f1702b0a1813e1457120fef4e28d
qianmoQ <shicheng@ttxit.com>
2022-09-19 19:11:00 +0800

    spi: Add jdbc column convert

---
+++ spi/src/main/java/io/edurt/datacap/spi/type/jdbc/JdbcColumnType.java
@@ -0,0 +1,19 @@
+package io.edurt.datacap.spi.type.jdbc;
+
+public enum JdbcColumnType
+{
+    INT,
+    TINYINT,
+    DOUBLE,
+    REAL,
+    INTEGER,
+    BIGINT,
+    SMALLINT,
+    DECIMAL,
+    BLOB,
+    DATE,
+    VARCHAR,
+    TIME,
+    BIT,
+    TIMESTAMP
+}
