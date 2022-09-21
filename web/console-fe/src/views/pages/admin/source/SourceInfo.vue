30d948806f797e3b22d1a552fd9f65247cb392b8
qianmoQ <shicheng@ttxit.com>
2022-09-22 00:38:37 +0800

    web: Support test source

--- web/console-fe/src/views/pages/admin/source/SourceInfo.vue
+++ web/console-fe/src/views/pages/admin/source/SourceInfo.vue
@@ -1,11 +1,12 @@
 <template>
   <a-modal v-model:visible="visible" :title="title" :footer="null" width="60%">
-    <a-form :model="formState" :validate-messages="validateMessages"
-            v-bind="layout" @finish="handlerSave($event)">
+    <a-form :model="formState" :validate-messages="validateMessages" v-bind="layout" @finish="handlerSave($event)">
       <a-tabs v-model:activeKey="activeKey">
         <a-tab-pane key="type">
           <template #tab>
-            <span><appstore-outlined/> Type </span>
+            <span>
+              <appstore-outlined /> Type
+            </span>
           </template>
           <a-form-item :name="['type']" :rules="[{ required: true }]">
             <a-row :gutter="[16,16]">
@@ -19,10 +20,12 @@
         </a-tab-pane>
         <a-tab-pane :disabled="!formState.type" key="configure">
           <template #tab>
-            <span><setting-outlined/> Configure </span>
+            <span>
+              <setting-outlined /> Configure
+            </span>
           </template>
           <a-form-item :name="['name']" label="Name" :rules="[{ required: true }]">
-            <a-input v-model:value="formState.name"/>
+            <a-input v-model:value="formState.name" />
           </a-form-item>
           <a-form-item :name="['protocol']" label="Protocol" :rules="[{ required: true }]">
             <a-select ref="select" v-model:value="formState.protocol" style="width: 120px">
@@ -32,39 +35,44 @@
             </a-select>
           </a-form-item>
           <a-form-item :name="['host']" label="Host" :rules="[{ required: true }]">
-            <a-input v-model:value="formState.host"/>
+            <a-input v-model:value="formState.host" />
           </a-form-item>
           <a-form-item :name="['port']" label="Port" :rules="[{type: 'number', min: 0, max: 65535}]">
-            <a-input-number v-model:value="formState.port"/>
+            <a-input-number v-model:value="formState.port" />
           </a-form-item>
         </a-tab-pane>
         <a-tab-pane :disabled="!formState.type" key="authorization">
           <template #tab>
-            <span><lock-outlined/> Authorization </span>
+            <span>
+              <lock-outlined /> Authorization
+            </span>
           </template>
           <a-form-item :name="['username']" label="UserName">
-            <a-input v-model:value="formState.username"/>
+            <a-input v-model:value="formState.username" />
           </a-form-item>
           <a-form-item :name="['password']" label="Password">
-            <a-input v-model:value="formState.password"/>
+            <a-input v-model:value="formState.password" />
           </a-form-item>
         </a-tab-pane>
         <a-tab-pane :disabled="!formState.type" key="advanced">
           <template #tab>
-            <span><group-outlined/> Advanced </span>
+            <span>
+              <group-outlined /> Advanced
+            </span>
           </template>
           <a-form-item :name="['catalog']" label="Catalog">
-            <a-input v-model:value="formState.catalog"/>
+            <a-input v-model:value="formState.catalog" />
           </a-form-item>
           <a-form-item :name="['database']" label="Database">
-            <a-input v-model:value="formState.database"/>
+            <a-input v-model:value="formState.database" />
           </a-form-item>
         </a-tab-pane>
       </a-tabs>
       <a-form-item :wrapper-col="{ ...layout.wrapperCol, offset: 8 }">
         <a-space style="width: 100%">
           <a-button key="cancel" danger size="small" @click="handlerCancel()">Cancel</a-button>
-          <a-button key="submit" type="primary" size="small" html-type="submit">Save</a-button>
+          <a-button key="test" type="primary" size="small" @click="handlerTest()">Test</a-button>
+          <a-button key="submit" type="primary" size="small" :disabled="isCreated" html-type="submit">Save</a-button>
         </a-space>
       </a-form-item>
     </a-form>
@@ -72,11 +80,11 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, reactive, ref} from "vue";
-import {SourceModel} from "@/model/SourceModel";
-import {SourceService} from "@/services/SourceService";
-import {message} from "ant-design-vue";
-import {emptySource} from "@/views/pages/admin/source/SourceGenerate";
+import { defineComponent, reactive, ref } from "vue";
+import { SourceModel } from "@/model/SourceModel";
+import { SourceService } from "@/services/SourceService";
+import { message } from "ant-design-vue";
+import { emptySource } from "@/views/pages/admin/source/SourceGenerate";
 
 export default defineComponent({
   name: "SourceInfoView",
@@ -90,11 +98,10 @@ export default defineComponent({
       default: () => 0
     }
   },
-  setup()
-  {
+  setup() {
     const layout = {
-      labelCol: {span: 6},
-      wrapperCol: {span: 12},
+      labelCol: { span: 6 },
+      wrapperCol: { span: 12 },
     };
     const validateMessages = {
       required: '${label} is required!',
@@ -111,14 +118,13 @@ export default defineComponent({
     };
   },
   components: {},
-  data()
-  {
+  data() {
     return {
-      title: ''
+      title: '',
+      isCreated: true
     }
   },
-  created()
-  {
+  created() {
     if (this.id <= 0) {
       this.title = 'Create New Source'
     }
@@ -127,12 +133,10 @@ export default defineComponent({
     }
   },
   methods: {
-    handlerCancel()
-    {
+    handlerCancel() {
       this.visible = false;
     },
-    handlerSave(values: SourceModel)
-    {
+    handlerSave(values: SourceModel) {
       new SourceService()
         .saveAndUpdate(values)
         .then((response) => {
@@ -141,16 +145,26 @@ export default defineComponent({
             this.visible = false;
           }
         });
+    },
+    handlerTest() {
+      new SourceService()
+        .testConnection(this.formState)
+        .then((response) => {
+          if (response.status) {
+            message.success("Test successful");
+            this.isCreated = false;
+          } else {
+            message.error(response.message);
+          }
+        });
     }
   },
   computed: {
     visible: {
-      get(): boolean
-      {
+      get(): boolean {
         return this.isVisible
       },
-      set(value: boolean)
-      {
+      set(value: boolean) {
         this.$emit('close', value)
       }
     }
