2059915a5e853c243febd81b734edba54c6646e5
qianmoQ <shicheng@ttxit.com>
2022-09-22 19:39:44 +0800

    web: Optimize code editor

--- web/console-fe/vue.config.js
+++ web/console-fe/vue.config.js
@@ -1,12 +1,16 @@
-const {defineConfig} = require('@vue/cli-service');
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const { defineConfig } = require('@vue/cli-service');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
   configureWebpack: {
     plugins: [
-      new MonacoWebpackPlugin()
+      new MonacoWebpackPlugin({
+        languages: ['sql'],
+      }),
+      new BundleAnalyzerPlugin()
     ]
   }
 })
