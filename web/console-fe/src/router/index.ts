e676eaa24123ab7ca64515407b3c8fc31aa69c87
qianmoQ <shicheng@ttxit.com>
2022-09-22 18:49:36 +0800

    web: Add a page loading progress bar

--- web/console-fe/src/router/index.ts
+++ web/console-fe/src/router/index.ts
@@ -1,6 +1,17 @@
 import LayoutContainer from "@/views/layout/Layout.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+NProgress.configure({
+  easing: 'ease',
+  speed: 600,
+  showSpinner: true,
+  trickleSpeed: 200,
+  minimum: 0.3
+});
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
@@ -63,12 +74,17 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
+  NProgress.start();
   if (to.matched.length === 0) {
     next({ name: "routerNotFound" })
   }
   else {
-    next()
+    next();
   }
 })
+
+router.afterEach(() => {
+  NProgress.done();
+});
 
 export default router;
