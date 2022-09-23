f6d3a71ef2df91428222768186be3d47aee998d8
qianmoQ <shicheng@ttxit.com>
2022-09-23 19:07:04 +0800

    web: Fix editor init failed

--- web/console-fe/vue.config.js
+++ web/console-fe/vue.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require('@vue/cli-service');
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
   configureWebpack: {
     plugins: [
-      new MonacoWebpackPlugin({
-        languages: ['sql'],
-      })
     ]
   },
   chainWebpack: (config) => {
