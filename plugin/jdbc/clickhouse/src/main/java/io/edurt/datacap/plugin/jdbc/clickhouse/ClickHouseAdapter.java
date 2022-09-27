8dba0be048165dc98979864adf38b07035965a18
qianmoQ <shicheng@ttxit.com>
2022-09-27 23:38:16 +0800

    plugin: Adapt the new plug-in mechanism for clickhouse

--- plugin/jdbc/clickhouse/src/main/java/io/edurt/datacap/plugin/jdbc/clickhouse/ClickHouseAdapter.java
+++ plugin/jdbc/clickhouse/src/main/java/io/edurt/datacap/plugin/jdbc/clickhouse/ClickHouseAdapter.java
@@ -1,10 +1,13 @@
 package io.edurt.datacap.plugin.jdbc.clickhouse;
 
 import io.edurt.datacap.spi.adapter.JdbcAdapter;
-import lombok.extern.slf4j.Slf4j;
+import io.edurt.datacap.spi.connection.JdbcConnection;
 
-@Slf4j
 public class ClickHouseAdapter
         extends JdbcAdapter
 {
+    public ClickHouseAdapter(JdbcConnection jdbcConnection)
+    {
+        super(jdbcConnection);
+    }
 }
