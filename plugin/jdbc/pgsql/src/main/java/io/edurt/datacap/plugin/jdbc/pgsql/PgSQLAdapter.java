c491164a157670642cae40f187cc9d41ba7090c6
马良 <mlboy@126.com>
2022-09-27 21:11:14 +0800

    增加pgsql驱动

---
+++ plugin/jdbc/pgsql/src/main/java/io/edurt/datacap/plugin/jdbc/pgsql/PgSQLAdapter.java
@@ -0,0 +1,10 @@
+package io.edurt.datacap.plugin.jdbc.pgsql;
+
+import io.edurt.datacap.spi.adapter.JdbcAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PgSQLAdapter
+        extends JdbcAdapter
+{
+}
