d17af8995a1bb22ac0bfab1ca82e6522038335bc
qianmoQ <shicheng@ttxit.com>
2022-10-15 19:24:32 +0800

    web: Optimizing build data sources

--- web/console-fe/src/views/pages/admin/source/SourceInfo.vue
+++ web/console-fe/src/views/pages/admin/source/SourceInfo.vue
@@ -1,18 +1,63 @@
 <template>
   <a-modal v-model:visible="visible" :title="title" :footer="null" width="60%" :closable="false">
+    <a-row :gutter="16">
+      <a-col :span="4">
+      </a-col>
+      <a-col :span="5">
+        <a-card :bordered="false" style="text-align: center" :bodyStyle="{'padding': '5px'}">
+          <a-card-meta>
+            <template #title>
+              <a-avatar :size="40" :style="{'background-color': !testInfo.percent ? '#CCC' : testInfo.connected ? '#52c41a' : '#ff4d4f'}">
+                <template #icon>
+                  <question-circle-outlined/>
+                </template>
+              </a-avatar>
+            </template>
+            <template #description>{{ !formState['type'] ? '_' : formState['type'] }}</template>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+      <a-col :span="6">
+        <a-card :bordered="false" style="text-align: center" :bodyStyle="{'padding': '5px'}">
+          <a-card-meta>
+            <template #description>
+              <a-progress :percent="testInfo.percent" :show-info="false"
+                          :status="testInfo.connected ? 'success' : 'exception'">
+              </a-progress>
+            </template>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+      <a-col :span="5">
+        <a-card :bordered="false" style="text-align: center" :bodyStyle="{'padding': '5px'}">
+          <a-card-meta>
+            <template #title>
+              <a-avatar :size="40" :style="{'background-color': !testInfo.percent ? '#CCC' : testInfo.connected ? '#52c41a' : '#ff4d4f'}">
+                <template #icon>
+                  <database-outlined/>
+                </template>
+              </a-avatar>
+            </template>
+            <template #description>Database</template>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+      <a-col :span="3">
+      </a-col>
+    </a-row>
     <a-form :model="formState" :validate-messages="validateMessages" v-bind="layout" @finish="handlerSave()">
-      <a-tabs v-model:activeKey="activeKey">
-        <a-tab-pane key="type">
+      <a-tabs v-model:activeKey="activeKey" centered>
+        <a-tab-pane key="type" style="text-align: center;">
           <template #tab>
             <span>
-              <appstore-outlined /> Type
+              <appstore-outlined/> Type
             </span>
           </template>
           <a-radio-group v-model:value="formState.type">
             <a-row :gutter="[16,8]">
               <a-col v-for="plugin in plugins" :span="6" v-bind:key="plugin.name">
                 <a-radio-button :value="plugin.name">
-                  {{plugin.name}}
+                  {{ plugin.name }}
                 </a-radio-button>
               </a-col>
             </a-row>
@@ -21,11 +66,11 @@
         <a-tab-pane :disabled="!formState.type" key="configure">
           <template #tab>
             <span>
-              <setting-outlined /> Configure
+              <setting-outlined/> Configure
             </span>
           </template>
           <a-form-item :name="['name']" label="Name" :rules="[{ required: true }]">
-            <a-input v-model:value="formState.name" />
+            <a-input v-model:value="formState.name"/>
           </a-form-item>
           <!-- <a-form-item :name="['protocol']" label="Protocol" :rules="[{ required: true }]">
             <a-select ref="select" v-model:value="formState.protocol" style="width: 120px">
@@ -35,36 +80,36 @@
             </a-select>
           </a-form-item> -->
           <a-form-item :name="['host']" label="Host" :rules="[{ required: true }]">
-            <a-input v-model:value="formState.host" />
+            <a-input v-model:value="formState.host"/>
           </a-form-item>
           <a-form-item :name="['port']" label="Port" :rules="[{type: 'number', min: 0, max: 65535}]">
-            <a-input-number v-model:value="formState.port" />
+            <a-input-number v-model:value="formState.port"/>
           </a-form-item>
         </a-tab-pane>
         <a-tab-pane :disabled="!formState.type" key="authorization">
           <template #tab>
             <span>
-              <lock-outlined /> Authorization
+              <lock-outlined/> Authorization
             </span>
           </template>
           <a-form-item :name="['username']" label="UserName">
-            <a-input v-model:value="formState.username" />
+            <a-input v-model:value="formState.username"/>
           </a-form-item>
           <a-form-item :name="['password']" label="Password">
-            <a-input v-model:value="formState.password" />
+            <a-input v-model:value="formState.password"/>
           </a-form-item>
         </a-tab-pane>
         <a-tab-pane :disabled="!formState.type" key="advanced">
           <template #tab>
             <span>
-              <group-outlined /> Advanced
+              <group-outlined/> Advanced
             </span>
           </template>
           <a-form-item :name="['catalog']" label="Catalog">
-            <a-input v-model:value="formState.catalog" />
+            <a-input v-model:value="formState.catalog"/>
           </a-form-item>
           <a-form-item :name="['database']" label="Database">
-            <a-input v-model:value="formState.database" />
+            <a-input v-model:value="formState.database"/>
           </a-form-item>
         </a-tab-pane>
       </a-tabs>
@@ -74,7 +119,7 @@
           </a-button>
           <a-button key="test" type="primary" size="small" :loading="connectionLoading" @click="handlerTest()">Test
           </a-button>
-          <a-button key="submit" type="primary" size="small" :disabled="isCreated" html-type="submit">Save</a-button>
+          <a-button key="submit" type="primary" size="small" :disabled="!testInfo.connected" html-type="submit">Save</a-button>
         </a-space>
       </a-form-item>
     </a-form>
@@ -82,11 +127,17 @@
 </template>
 
 <script lang="ts">
-import { SourceModel } from "@/model/SourceModel";
-import { SourceService } from "@/services/SourceService";
-import { emptySource } from "@/views/pages/admin/source/SourceGenerate";
-import { message } from "ant-design-vue";
-import { defineComponent, reactive, ref } from "vue";
+import {SourceModel} from "@/model/SourceModel";
+import {SourceService} from "@/services/SourceService";
+import {emptySource} from "@/views/pages/admin/source/SourceGenerate";
+import {message} from "ant-design-vue";
+import {defineComponent, reactive, ref} from "vue";
+
+interface TestInfo
+{
+  connected: boolean,
+  percent: number
+}
 
 export default defineComponent({
   name: "SourceInfoView",
@@ -100,10 +151,11 @@ export default defineComponent({
       default: () => 0
     }
   },
-  setup() {
+  setup()
+  {
     const layout = {
-      labelCol: { span: 6 },
-      wrapperCol: { span: 12 },
+      labelCol: {span: 6},
+      wrapperCol: {span: 12},
     };
     const validateMessages = {
       required: '${label} is required!',
@@ -119,17 +171,19 @@ export default defineComponent({
     };
   },
   components: {},
-  data() {
+  data()
+  {
     return {
       title: '',
-      isCreated: true,
       isUpdate: false,
       formState: {},
       plugins: [],
-      connectionLoading: false
+      testInfo: {} as TestInfo,
+      connectionLoading: false,
     }
   },
-  created() {
+  created()
+  {
     if (this.id <= 0) {
       this.title = 'Create New Source';
       this.formState = reactive(emptySource);
@@ -141,7 +195,8 @@ export default defineComponent({
     this.handlerInitialize();
   },
   methods: {
-    handlerInitialize() {
+    handlerInitialize()
+    {
       if (this.id > 0) {
         new SourceService().getById(this.id)
           .then(response => {
@@ -157,10 +212,12 @@ export default defineComponent({
           }
         });
     },
-    handlerCancel() {
+    handlerCancel()
+    {
       this.visible = false;
     },
-    handlerSave() {
+    handlerSave()
+    {
       new SourceService()
         .saveAndUpdate(this.formState as SourceModel, this.isUpdate)
         .then((response) => {
@@ -170,28 +227,35 @@ export default defineComponent({
           }
         });
     },
-    handlerTest() {
+    handlerTest()
+    {
       this.connectionLoading = true;
       new SourceService()
         .testConnection(this.formState as SourceModel)
         .then((response) => {
+          this.testInfo.percent = 100;
           if (response.status) {
             message.success("Test successful");
-            this.isCreated = false;
-          } else {
-            message.error(response.message);
+            this.testInfo.connected = true;
           }
-        }).finally(() => {
+          else {
+            message.error(response.message);
+            this.testInfo.connected = false;
+          }
+        })
+        .finally(() => {
           this.connectionLoading = false;
         });
     }
   },
   computed: {
     visible: {
-      get(): boolean {
+      get(): boolean
+      {
         return this.isVisible
       },
-      set(value: boolean) {
+      set(value: boolean)
+      {
         this.$emit('close', value)
       }
     }
