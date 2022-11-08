83ef4aaa75105f073d80ec08392cf422c5a3ed14
qianmoQ <shicheng@ttxit.com>
2022-11-08 23:12:35 +0800

    fix: It is difficult to cancel the automatic prompt (close #129)

--- web/console-fe/src/views/pages/console/ConsoleOnline.vue
+++ web/console-fe/src/views/pages/console/ConsoleOnline.vue
@@ -219,7 +219,7 @@ export default defineComponent({
             }))
           };
         },
-        triggerCharacters: ['.', ' '],
+        triggerCharacters: ['.'],
       });
     },
     handlerRun()
