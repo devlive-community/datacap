064f73d5414465fff2433770da4e1ab8709abed2
qianmoQ <shicheng@ttxit.com>
2022-11-26 22:21:44 +0800

    [Web] Optimize about page

--- web/console-fe/src/views/common/CommonAbout.vue
+++ web/console-fe/src/views/common/CommonAbout.vue
@@ -1,27 +1,33 @@
 <template>
   <div>
+    <Alert type="success" banner show-icon>Released Note</Alert>
     <Timeline pending class="datacap-release-note">
-      <TimelineItem v-for="releaseNote of ReleaseNotes.releaseNotes" v-bind:key="releaseNote.version">
-        <p class="time">
-          <Tag :color="releaseNote.latest ? 'primary' : 'default'">{{ releaseNote.version }}</Tag>
-          <Tag color="default">{{ releaseNote.time }}</Tag>
-        </p>
-        <p class="content">
-          <vue-markdown-it :source="releaseNote.content"/>
-        </p>
+      <TimelineItem :color="releaseNote.latest ? 'blue' : 'green'"
+                    v-for="releaseNote of ReleaseNotes.releaseNotes" v-bind:key="releaseNote.version">
+        <template #dot>
+          <Icon :type="releaseNote.latest ? 'md-trophy' : 'md-archive'" :size="20"></Icon>
+        </template>
+        <div style="margin-top: -5px;">
+          <p class="time">
+            <Space>
+              <Tag :color="releaseNote.latest ? 'primary' : 'success'">{{ releaseNote.version }}</Tag>
+              <Tag type="border" :color="releaseNote.latest ? 'primary' : 'success'">{{ releaseNote.time }}</Tag>
+              <Time :time="releaseNote.time"/>
+            </Space>
+          </p>
+          <p class="content">
+            <v-md-preview :text="releaseNote.content"/>
+          </p>
+        </div>
       </TimelineItem>
     </Timeline>
   </div>
 </template>
 <script lang="ts">
-import VueMarkdownIt from 'vue3-markdown-it';
 import 'highlight.js/styles/monokai.css';
 import ReleaseNotes from "@/views/common/release/ReleaseNotes";
 
 export default {
-  components: {
-    VueMarkdownIt
-  },
   setup()
   {
     return {
