063cbb95778c33181821241841b002a541431b6b
qianmoQ <shicheng@ttxit.com>
2022-12-05 17:03:51 +0800

    [Plugin] Support neo4j for jdbc

---
+++ plugin/jdbc/neo4j/src/main/java/io/edurt/datacap/plugin/jdbc/neo4j/Neo4jPlugin.java
@@ -0,0 +1,83 @@
+package io.edurt.datacap.plugin.jdbc.neo4j;
+
+import io.edurt.datacap.spi.Plugin;
+import io.edurt.datacap.spi.PluginType;
+import io.edurt.datacap.spi.adapter.JdbcAdapter;
+import io.edurt.datacap.spi.connection.JdbcConfigure;
+import io.edurt.datacap.spi.connection.JdbcConnection;
+import io.edurt.datacap.spi.model.Configure;
+import io.edurt.datacap.spi.model.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.ObjectUtils;
+
+@Slf4j
+public class Neo4jPlugin
+        implements Plugin
+{
+    private JdbcConfigure jdbcConfigure;
+    private JdbcConnection connection;
+    private Response response;
+
+    @Override
+    public String validator()
+    {
+        return "CALL dbms.components()";
+    }
+
+    @Override
+    public String name()
+    {
+        return "Neo4j";
+    }
+
+    @Override
+    public String description()
+    {
+        return "Integrate Neo4j data sources";
+    }
+
+    @Override
+    public PluginType type()
+    {
+        return PluginType.JDBC;
+    }
+
+    @Override
+    public void connect(Configure configure)
+    {
+        try {
+            this.response = new Response();
+            this.jdbcConfigure = new JdbcConfigure();
+            BeanUtils.copyProperties(this.jdbcConfigure, configure);
+            this.jdbcConfigure.setJdbcDriver("org.neo4j.driver.Driver");
+            this.jdbcConfigure.setJdbcType("neo4j:bolt");
+            this.connection = new JdbcConnection(this.jdbcConfigure, this.response);
+        }
+        catch (Exception ex) {
+            this.response.setIsConnected(Boolean.FALSE);
+            this.response.setMessage(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Response execute(String content)
+    {
+        if (ObjectUtils.isNotEmpty(this.connection)) {
+            log.info("Execute neo4j plugin logic started");
+            this.response = this.connection.getResponse();
+            JdbcAdapter processor = new Neo4jAdapter(this.connection);
+            this.response = processor.handlerExecute(content);
+            log.info("Execute neo4j plugin logic end");
+        }
+        return this.response;
+    }
+
+    @Override
+    public void destroy()
+    {
+        if (ObjectUtils.isNotEmpty(this.connection)) {
+            this.connection.destroy();
+        }
+    }
+}
