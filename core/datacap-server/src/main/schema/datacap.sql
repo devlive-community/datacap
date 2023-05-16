9fb1215345a042cef8dbbf9d54a165ed8e3d7ac1
qianmoQ <shicheng@ttxit.com>
2023-05-16 21:37:35 +0800

    [Core] [Manager] Improve the mysql metadata management to obtain type

--- core/datacap-server/src/main/schema/datacap.sql
+++ core/datacap-server/src/main/schema/datacap.sql
@@ -289,7 +289,8 @@ VALUES ('datacap', '$2a$10$bZ4XBRlYUjKfkBovWT9TuuXlEF7lpRxVrXS8iqyCjCHUqy4RPTL8.
 -- --------------------------------
 --       Update to 1.10.0        --
 -- --------------------------------
-INSERT INTO datacap.template_sql(name, content, description, plugin, configure, `system`)
+/* H2 */
+INSERT INTO template_sql(name, content, description, plugin, configure, `system`)
 VALUES ('FindTableTypeByDatabase', 'SELECT
   CASE
     INFORMATION_SCHEMA.TABLES.TABLE_TYPE
@@ -301,7 +302,7 @@ FROM
 WHERE
   INFORMATION_SCHEMA.TABLES.TABLE_SCHEMA = ''${database:String}''
 GROUP BY
-  table_type', 'Finds all table types under the database according to the database', 'H2,MySQL', '[{"column":"database","type":"String","expression":"${database:String}"}]', TRUE),
+  table_type', 'Finds all table types under the database according to the database', 'H2', '[{"column":"database","type":"String","expression":"${database:String}"}]', TRUE),
        ('FindTableByDatabaseAndType', 'SELECT
   TABLE_CATALOG AS DATABASE_NAME,
   TABLE_NAME AS TABLE_NAME
@@ -316,7 +317,7 @@ WHERE
   END = ''${type:String}''
 GROUP BY
   TABLE_NAME,
-  TABLE_CATALOG', 'Gets a collection of related data based on the specified database and data type', 'H2,MySQL',
+  TABLE_CATALOG', 'Gets a collection of related data based on the specified database and data type', 'H2',
         '[{"column":"database","type":"String","expression":"${database:String}"},{"column":"type","type":"String","expression":"${type:String}"}]', TRUE),
        ('FindColumnTypeByDatabaseAndTable', 'SELECT
   ''${database:String}'' AS TABLE_CATALOG,
@@ -425,4 +426,143 @@ GROUP BY
 ORDER BY
   COLUMN_NAME', 'Gets a collection of related data based on the specified database, table, and data type', 'H2',
         '[{"column":"database","type":"String","expression":"${database:String}"},{"column":"table","type":"String","expression":"${table:String}"},{"column":"type","type":"String","expression":"${type:String}"}]',
-        TRUE)
+        TRUE);
+/* MySQL */
+INSERT INTO template_sql(name, content, description, plugin, configure, `system`)
+VALUES ('FindTableTypeByDatabase', 'SELECT
+  CASE
+    INFORMATION_SCHEMA.TABLES.TABLE_TYPE
+    WHEN ''VIEW'' THEN ''view''
+    ELSE ''table''
+  END AS table_type
+FROM
+  INFORMATION_SCHEMA.TABLES
+WHERE
+  INFORMATION_SCHEMA.TABLES.TABLE_SCHEMA = ''${database:String}''
+GROUP BY
+  table_type', 'Finds all table types under the database according to the database', 'MySQL', '[{"column":"database","type":"String","expression":"${database:String}"}]', TRUE),
+       ('FindTableByDatabaseAndType', 'SELECT
+  TABLE_SCHEMA AS TABLE_CATALOG,
+  TABLE_NAME AS TABLE_NAME
+FROM
+  INFORMATION_SCHEMA.TABLES
+WHERE
+  INFORMATION_SCHEMA.TABLES.TABLE_SCHEMA = ''${database:String}''
+  AND CASE
+    INFORMATION_SCHEMA.TABLES.TABLE_TYPE
+    WHEN ''VIEW'' THEN ''view''
+    ELSE ''table''
+  END = ''${type:String}''
+GROUP BY
+  TABLE_NAME,
+  TABLE_SCHEMA', 'Gets a collection of related data based on the specified database and data type', 'MySQL',
+        '[{"column":"database","type":"String","expression":"${database:String}"},{"column":"type","type":"String","expression":"${type:String}"}]', TRUE),
+       ('FindColumnTypeByDatabaseAndTable', 'SELECT
+  ''${database:String}'' AS TABLE_CATALOG,
+  COLUMN_TYPE
+FROM
+  (
+    SELECT
+      CASE
+        WHEN (
+          icl.IS_INDEX = ''Y''
+          AND col.COLUMN_KEY = ''PRI''
+        ) THEN ''index,primaryKey''
+        WHEN col.COLUMN_KEY = ''PRI'' THEN ''primaryKey''
+        WHEN icl.IS_INDEX = ''Y'' THEN ''index''
+        ELSE ''column''
+      END AS COLUMN_TYPE
+    FROM
+      INFORMATION_SCHEMA.COLUMNS col
+      LEFT JOIN (
+        SELECT
+          TABLE_SCHEMA,
+          TABLE_NAME,
+          COLUMN_NAME,
+          ''Y'' AS IS_KEY
+        FROM
+          INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+      ) kcu ON kcu.TABLE_SCHEMA = col.TABLE_SCHEMA
+      AND kcu.TABLE_NAME = col.TABLE_NAME
+      AND kcu.COLUMN_NAME = col.COLUMN_NAME
+      LEFT JOIN (
+        SELECT
+          TABLE_SCHEMA AS TABLE_SCHEMA,
+          TABLE_NAME AS TABLE_NAME,
+          COLUMN_NAME AS COLUMN_NAME,
+          ''Y'' AS IS_INDEX
+        FROM
+          INFORMATION_SCHEMA.`STATISTICS`
+        WHERE
+          TABLE_CATALOG = ''${database:String}''
+          AND TABLE_NAME = ''${table:String}''
+      ) icl ON icl.TABLE_SCHEMA = col.TABLE_SCHEMA
+      AND icl.TABLE_NAME = col.TABLE_NAME
+      AND icl.COLUMN_NAME = col.COLUMN_NAME
+    WHERE
+      col.TABLE_SCHEMA = ''${database:String}''
+      AND col.TABLE_NAME = ''${table:String}''
+    ORDER BY
+      col.COLUMN_NAME
+  ) AS tmp
+GROUP BY
+  COLUMN_TYPE', 'Gets the data column classification collection based on the provided database and data table', 'MySQL',
+        '[{"column":"database","type":"String","expression":"${database:String}"},{"column":"table","type":"String","expression":"${table:String}"}]', TRUE),
+       ('FindColumnByDatabaseAndTableAndType', 'SELECT
+  COLUMN_NAME,
+  COLUMN_TYPE
+FROM
+  (
+    SELECT
+      col.COLUMN_NAME AS COLUMN_NAME,
+      CASE
+        WHEN (
+          icl.IS_INDEX = ''Y''
+          AND col.COLUMN_KEY = ''PRI''
+        ) THEN ''index,primaryKey''
+        WHEN col.COLUMN_KEY = ''PRI'' THEN ''primaryKey''
+        WHEN icl.IS_INDEX = ''Y'' THEN ''index''
+        ELSE ''column''
+      END AS COLUMN_TYPE
+    FROM
+      INFORMATION_SCHEMA.COLUMNS col
+      LEFT JOIN (
+        SELECT
+          TABLE_SCHEMA,
+          TABLE_NAME,
+          COLUMN_NAME,
+          ''Y'' AS IS_KEY
+        FROM
+          INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+      ) kcu ON kcu.TABLE_SCHEMA = col.TABLE_SCHEMA
+      AND kcu.TABLE_NAME = col.TABLE_NAME
+      AND kcu.COLUMN_NAME = col.COLUMN_NAME
+      LEFT JOIN (
+        SELECT
+          TABLE_SCHEMA AS TABLE_SCHEMA,
+          TABLE_NAME AS TABLE_NAME,
+          COLUMN_NAME AS COLUMN_NAME,
+          ''Y'' AS IS_INDEX
+        FROM
+          INFORMATION_SCHEMA.`STATISTICS`
+        WHERE
+          TABLE_CATALOG = ''${database:String}''
+          AND TABLE_NAME = ''${table:String}''
+      ) icl ON icl.TABLE_SCHEMA = col.TABLE_SCHEMA
+      AND icl.TABLE_NAME = col.TABLE_NAME
+      AND icl.COLUMN_NAME = col.COLUMN_NAME
+    WHERE
+      col.TABLE_SCHEMA = ''${database:String}''
+      AND col.TABLE_NAME = ''${table:String}''
+    ORDER BY
+      col.COLUMN_NAME
+  ) AS tmp
+WHERE
+  COLUMN_TYPE LIKE ''%${type:String}%''
+GROUP BY
+  COLUMN_NAME,
+  COLUMN_TYPE
+ORDER BY
+  COLUMN_NAME', 'Gets a collection of related data based on the specified database, table, and data type', 'MySQL',
+        '[{"column":"database","type":"String","expression":"${database:String}"},{"column":"table","type":"String","expression":"${table:String}"},{"column":"type","type":"String","expression":"${type:String}"}]',
+        TRUE);
