29aa3c9607e89ae6af9da235a8366ce600f2a0cd
qianmoQ <shicheng@devlive.org>
2023-06-13 10:15:10 +0800

    [Core] [Executor] Update executor name

--- core/datacap-spi/src/main/java/io/edurt/datacap/spi/executor/ExecutorModule.java
+++ core/datacap-spi/src/main/java/io/edurt/datacap/spi/executor/ExecutorModule.java
@@ -5,4 +5,11 @@ import com.google.inject.AbstractModule;
 public class ExecutorModule
         extends AbstractModule
 {
+    String name()
+    {
+        // Intercept the suffix Executor to mark the executor name
+        return this.getClass().getSimpleName()
+                .replace("Executor", "")
+                .replace("Module", "");
+    }
 }
