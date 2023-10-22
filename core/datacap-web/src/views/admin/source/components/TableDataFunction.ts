d29b6e54b3740aac2f6b9ed4c13aa9628623abef
qianmoQ <shicheng@devlive.org>
2023-10-22 15:49:04 +0800

    [Metadata] [Editor] Prevents the front-end from reordering data

--- core/datacap-web/src/views/admin/source/components/TableDataFunction.ts
+++ core/datacap-web/src/views/admin/source/components/TableDataFunction.ts
@@ -13,7 +13,8 @@ const createColumnDefs = (headers: any[], types: any[]): any[] => {
     const columnDef = {
       headerName: header,
       field: header,
-      headerTooltip: header + ' [' + types[index] + ']'
+      headerTooltip: header + ' [' + types[index] + ']',
+      unSortIcon: true
     };
     columnDefs.push(columnDef);
   })
@@ -32,10 +33,14 @@ const createDataEditorOptions = (i18n: any): object => {
     localeText: TableGridOptions.createLocale(i18n),
     // Fixed issues: https://github.com/EdurtIO/datacap/issues/219
     suppressFieldDotNotation: true,
+    // Turn on multi-column sorting
+    alwaysMultiSort: true,
     defaultColDef: {
       sortable: true,
       resizable: true,
-      wrapText: true
+      wrapText: true,
+      // Prevents the front-end from reordering data
+      comparator: (valueA, valueB, nodeA, nodeB, isDescending) => 0
     }
   }
   return gridOptions;
