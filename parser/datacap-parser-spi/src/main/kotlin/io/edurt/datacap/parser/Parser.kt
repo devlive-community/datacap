5c38c4f3f773097c53c8d31b52f471b8049d351d
qianmoQ <shicheng@devlive.org>
2023-12-29 23:16:32 +0800

    [Core] [Parser] Add parser spi

---
+++ parser/datacap-parser-spi/src/main/kotlin/io/edurt/datacap/parser/Parser.kt
@@ -0,0 +1,21 @@
+package io.edurt.datacap.parser
+
+interface Parser {
+    fun name(): String {
+        return this.javaClass
+                .simpleName
+                .removeSuffix("Parser")
+    }
+
+    fun description(): String {
+        return "Integrate ${name()} sql parser"
+    }
+
+    /**
+     * Parses the given SQL string and returns a ParserInfo object.
+     *
+     * @param sql the SQL string to parse
+     * @return    the ParserInfo object containing the parsed information
+     */
+    fun parse(sql: String): ParserResponse
+}
---
+++ parser/datacap-parser-spi/src/test/kotlin/io/edurt/datacap/parser/TestParser.kt
@@ -0,0 +1,8 @@
+package io.edurt.datacap.parser
+
+class TestParser : Parser {
+    override fun parse(sql: String): ParserResponse {
+        val response = ParserResponse()
+        return response
+    }
+}
