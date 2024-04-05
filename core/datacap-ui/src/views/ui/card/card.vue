b9c9bebfdd663d5bd1ebeb440eecd25941a2bfbe
qianmoQ <shicheng@devlive.org>
2024-04-05 17:33:00 +0800

    [Core] [Refactor] [UI] Add card component

---
+++ core/datacap-ui/src/views/ui/card/card.vue
@@ -0,0 +1,35 @@
+<template>
+  <Card>
+    <CardHeader class="border-b p-4">
+      <CardHeader class="flex flex-row items-center border-b p-4">
+        <div class="grid gap-2">
+          <CardTitle>
+            <span v-if="title">{{ title }}</span>
+            <slot v-else name="title"/>
+          </CardTitle>
+        </div>
+        <slot name="extra"/>
+      </CardHeader>
+    </CardHeader>
+    <CardContent>
+      <slot/>
+    </CardContent>
+  </Card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default defineComponent({
+  name: 'DcCard',
+  components: {
+    Card, CardContent, CardHeader, CardTitle
+  },
+  props: {
+    title: {
+      type: String
+    }
+  }
+})
+</script>
