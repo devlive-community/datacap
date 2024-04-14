31b3c172d4a9107c43cd3c837820fd679f0b7c0b
qianmoQ <shicheng@devlive.org>
2024-04-14 22:44:26 +0800

    [Core] [UI] Remove echarts

--- core/datacap-ui/src/views/components/echarts/EchartsConfigure.ts
+++
@@ -1,9 +0,0 @@
-import { EchartsType } from '@/views/components/echarts/EchartsType'
-
-export class EchartsConfigure
-{
-    headers: [] | undefined
-    types: [] | undefined
-    columns: [] | undefined
-    type: EchartsType = EchartsType.LINE
-}
--- core/datacap-ui/src/views/components/echarts/configure/AxisConfigure.ts
+++
@@ -1,9 +0,0 @@
-export class AxisConfigure
-{
-    type = 'category'
-    data: never[] = []
-    disabled = false
-    meta = {
-        column: null as string | null
-    }
-}
