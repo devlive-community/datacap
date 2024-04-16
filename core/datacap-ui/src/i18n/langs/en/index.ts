2895d96713600e1be09ac524ed801993d68c776c
qianmoQ <shicheng@devlive.org>
2024-04-16 20:31:36 +0800

    [Core] [UI] Merge query

--- core/datacap-ui/src/i18n/langs/en/index.ts
+++ core/datacap-ui/src/i18n/langs/en/index.ts
@@ -1,9 +1,26 @@
 import state from '@/i18n/langs/en/state'
-import query from '@/i18n/langs/en/query'
 
 export default {
     state: state,
-    query: query,
+    query: {
+        common: {
+            execute: 'Execute',
+            executeSelection: 'Execute (Selection)',
+            connectionTime: 'Connection Time',
+            executionTime: 'Execution Time',
+            format: 'Format',
+            help: 'Help',
+            showSql: 'Show SQL',
+            quoteRecord: 'Quote Record',
+            historyData: 'History Data',
+            historyDataInfo: '[ $VALUE ] History Data'
+        },
+        tip: {
+            pageShow: 'Open / Close the page',
+            smallTips: 'Tip: hold down the keyboard Shift and select the number of data rows with the mouse to automatically copy the contents of the selected row',
+            notPresetToken: 'There is no token available, please go to your personal center to configure a token',
+        }
+    },
     source: {
         common: {
             list: 'Source List',
--- core/datacap-ui/src/i18n/langs/zhCn/index.ts
+++ core/datacap-ui/src/i18n/langs/zhCn/index.ts
@@ -1,9 +1,26 @@
 import state from '@/i18n/langs/zhCn/state'
-import query from '@/i18n/langs/zhCn/query'
 
 export default {
     state: state,
-    query: query,
+    query: {
+        common: {
+            execute: '执行',
+            executeSelection: '执行 (选中)',
+            connectionTime: '连接耗时',
+            executionTime: '执行耗时',
+            format: '格式化',
+            help: '帮助',
+            showSql: '显示 SQL',
+            quoteRecord: '引用记录',
+            historyData: '历史数据',
+            historyDataInfo: '[ $VALUE ] 历史数据',
+        },
+        tip: {
+            pageShow: '打开 / 关闭分页',
+            smallTips: '小技巧：按住键盘 Shift 鼠标选择数据行数会自动复制选中行内容',
+            notPresetToken: '暂无可用的 Token，请到个人中心配置 Token',
+        }
+    },
     source: {
         common: {
             list: '数据源列表',
