690bcb568b1d045de7f21c5ccfaec547c5eb97a7
qianmoQ <shicheng@devlive.org>
2024-04-16 20:18:27 +0800

    [Core] [UI] Merge pipeline

--- core/datacap-ui/src/i18n/langs/en/index.ts
+++ core/datacap-ui/src/i18n/langs/en/index.ts
@@ -8,7 +8,6 @@ import template from '@/i18n/langs/en/template'
 import menu from '@/i18n/langs/en/menu'
 import snippet from '@/i18n/langs/en/snippet'
 import report from '@/i18n/langs/en/report'
-import pipeline from '@/i18n/langs/en/pipeline'
 
 export default {
     state: state,
@@ -21,7 +20,39 @@ export default {
     menu: menu,
     snippet: snippet,
     report: report,
-    pipeline: pipeline,
+    pipeline: {
+        common: {
+            list: 'Pipeline List',
+            logger: 'Pipeline Logger',
+            loggerInfo: 'Pipeline [ $VALUE ] Logger',
+            delete: 'Delete Pipeline',
+            deleteInfo: 'Delete Pipeline [ $VALUE ]',
+            stop: 'Stop Pipeline',
+            stopInfo: 'Stop Pipeline [ $VALUE ]',
+            flow: 'Pipeline Flow',
+            flowInfo: 'Pipeline [ $VALUE ] Flow',
+            create: 'Create Pipeline',
+            input: 'Input Source',
+            output: 'Output Source',
+            resetTransform: 'Reset Transform',
+        },
+        validator: {
+            from: 'Please configure the input source information',
+            to: 'Please configure the output source information',
+            edge: 'Please connect the input and output source'
+        },
+        tip: {
+            deleteSuccess: 'Delete pipeline [ $VALUE ] successfully',
+            deleteAlert1: 'You are deleting a pipeline. This action permanently deletes the pipeline. Please be sure to confirm your actions before proceeding.',
+            deleteAlert2: 'Warning: This cannot be undone.',
+            deleteAlert3: 'To confirm, type [ $VALUE ] in the box below',
+            stopAlert1: 'You are stopping a pipeline. This action permanently stops the pipeline. Please be sure to confirm your actions before proceeding.',
+            stopAlert2: 'Warning: This cannot be undone.',
+            stopAlert3: 'To confirm, type [ $VALUE ] in the box below',
+            stopSuccess: 'Pipeline [ $VALUE ] stopped successfully',
+            publishSuccess: 'Pipeline [ $VALUE ] published successfully',
+        }
+    },
     common: {
         home: 'Home',
         dashboard: 'Dashboard',
--- core/datacap-ui/src/i18n/langs/zhCn/index.ts
+++ core/datacap-ui/src/i18n/langs/zhCn/index.ts
@@ -8,7 +8,6 @@ import template from '@/i18n/langs/zhCn/template'
 import menu from '@/i18n/langs/zhCn/menu'
 import snippet from '@/i18n/langs/zhCn/snippet'
 import report from '@/i18n/langs/zhCn/report'
-import pipeline from '@/i18n/langs/zhCn/pipeline.ts'
 
 export default {
     state: state,
@@ -21,7 +20,39 @@ export default {
     menu: menu,
     snippet: snippet,
     report: report,
-    pipeline: pipeline,
+    pipeline: {
+        common: {
+            list: '流水线列表',
+            logger: '流水线日志',
+            loggerInfo: '流水线 [ $VALUE ] 日志',
+            delete: '删除流水线',
+            deleteInfo: '删除流水线 [ $VALUE ]',
+            stop: '停止流水线',
+            stopInfo: '停止流水线 [ $VALUE ]',
+            flow: '流水线视图',
+            flowInfo: '流水线 [ $VALUE ] 视图',
+            create: '创建流水线',
+            input: '输入源',
+            output: '输出源',
+            resetTransform: '重置变换',
+        },
+        validator: {
+            from: '请配置输入源信息',
+            to: '请配置输出源信息',
+            edge: '请连接输入和输出源'
+        },
+        tip: {
+            deleteSuccess: '删除流水线 [ $VALUE ] 成功',
+            deleteAlert1: '您正在删除流水线。此操作将永久删除流水线。在继续操作之前，请务必确认您的操作。',
+            deleteAlert2: '警告：此操作无法撤销。',
+            deleteAlert3: '要确认，请在下面的框中键入 [ $VALUE ]',
+            stopAlert1: '您正在停止流水线。此操作将永久停止流水线。在继续操作之前，请务必确认您的操作。',
+            stopAlert2: '警告：此操作无法撤销。',
+            stopAlert3: '要确认，请在下面的框中键入 [ $VALUE ]',
+            stopSuccess: '流水线 [ $VALUE ] 停止成功',
+            publishSuccess: '流水线 [ $VALUE ] 发布成功',
+        }
+    },
     common: {
         home: '首页',
         dashboard: '仪表盘',
