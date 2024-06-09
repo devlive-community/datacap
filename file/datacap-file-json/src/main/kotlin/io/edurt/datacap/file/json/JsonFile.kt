e8ee704b9e3ac72cc49a88cc3ad9e3964a6a60cd
qianmoQ <shicheng@devlive.org>
2024-06-09 10:26:40 +0800

    [File] [Json] Support json

---
+++ file/datacap-file-json/src/main/kotlin/io/edurt/datacap/file/json/JsonFile.kt
@@ -0,0 +1,18 @@
+package io.edurt.datacap.file.json
+
+import io.edurt.datacap.file.File
+import io.edurt.datacap.file.model.FileRequest
+import io.edurt.datacap.file.model.FileResponse
+
+class JsonFile : File
+{
+    override fun writer(request: FileRequest): FileResponse
+    {
+        TODO("Not yet implemented")
+    }
+
+    override fun reader(request: FileRequest): FileResponse
+    {
+        TODO("Not yet implemented")
+    }
+}
