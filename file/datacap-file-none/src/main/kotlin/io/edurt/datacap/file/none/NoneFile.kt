e45f32a3bcacd786e2e87255433a45ae4c04db0d
qianmoQ <shicheng@devlive.org>
2024-06-10 08:46:03 +0800

    [Core] [File] Support format

--- file/datacap-file-none/src/main/kotlin/io/edurt/datacap/file/none/NoneFile.kt
+++ file/datacap-file-none/src/main/kotlin/io/edurt/datacap/file/none/NoneFile.kt
@@ -1,14 +1,37 @@
 package io.edurt.datacap.file.none
 
+import io.edurt.datacap.common.utils.DateUtils
 import io.edurt.datacap.file.File
 import io.edurt.datacap.file.model.FileRequest
 import io.edurt.datacap.file.model.FileResponse
+import org.slf4j.LoggerFactory.getLogger
+import java.io.IOException
 
 class NoneFile : File
 {
+    private val log = getLogger(this::class.java)
+
     override fun format(request: FileRequest): FileResponse
     {
-        TODO("Not yet implemented")
+        val response = FileResponse()
+        try
+        {
+            log.info("${name()} format start time [ ${DateUtils.now()} ]")
+            response.headers = request.headers
+            val columns = mutableListOf<Any>()
+            request.columns
+                .forEach { columns.add(it) }
+            response.columns = columns
+
+            log.info("${name()} format end time [ ${DateUtils.now()} ]")
+            response.successful = true
+        }
+        catch (e: IOException)
+        {
+            response.successful = false
+            response.message = e.message
+        }
+        return response
     }
 
     override fun writer(request: FileRequest): FileResponse
---
+++ file/datacap-file-none/src/test/kotlin/io/edurt/datacap/file/none/NoneFileTest.kt
@@ -0,0 +1,55 @@
+package io.edurt.datacap.file.none
+
+import com.google.inject.Guice.createInjector
+import com.google.inject.Injector
+import io.edurt.datacap.file.FileFilter
+import io.edurt.datacap.file.FileManager
+import io.edurt.datacap.file.model.FileRequest
+import org.junit.Before
+import org.junit.Test
+import org.slf4j.LoggerFactory.getLogger
+import kotlin.test.assertTrue
+
+class NoneFileTest
+{
+    private val log = getLogger(this::class.java)
+    private val name = "None"
+    private var injector: Injector? = null
+    private val request: FileRequest = FileRequest()
+
+    @Before
+    fun before()
+    {
+        injector = createInjector(FileManager())
+
+        request.name = "test"
+        request.path = System.getProperty("user.dir")
+        request.headers = listOf("name", "age")
+
+        val l1 = listOf("Test", 12)
+        val l2 = listOf("Test1", 121)
+        request.columns = listOf(l1, l2)
+    }
+
+    @Test
+    fun testFormat()
+    {
+        injector?.let { injector ->
+            FileFilter.findNotify(injector, name)
+                .ifPresent { file ->
+                    val response = file.format(request)
+                    log.info("headers: [ ${response.headers} ]")
+                    response.columns
+                        .let { columns ->
+                            columns.forEachIndexed { index, line ->
+                                log.info("index: [ $index ], line: [ $line ]")
+                            }
+                        }
+
+                    assertTrue {
+                        response.successful == true
+                    }
+                }
+        }
+    }
+}
