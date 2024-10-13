24f54a4979b20d245461a6782a76d7299a31ff9a
qianmoQ <shicheng@devlive.org>
2024-10-13 18:09:01 +0800

    [Core] [UI] Support loading for card component

--- core/datacap-ui/src/views/pages/default/dashboard/DashboardHome.vue
+++ core/datacap-ui/src/views/pages/default/dashboard/DashboardHome.vue
@@ -2,25 +2,24 @@
   <div class="hidden flex-col md:flex">
     <div class="flex-1 space-y-4 p-8 pt-6">
       <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <Card>
-          <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle class="text-sm font-medium">{{ $t('user.common.sourceCount') }}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Loader2 v-if="loading" class="w-4 h-4 mr-2 animate-spin text-center"></Loader2>
-            <div class="text-2xl font-bold">{{ summary.sourceCount }}</div>
-            <p class="text-xs text-muted-foreground mt-2">{{ $t('user.tip.sourceCountTip') }}</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle class="text-sm font-medium">{{ $t('user.common.queryCount') }}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div class="text-2xl font-bold">{{ summary.queryCount }}</div>
-            <p class="text-xs text-muted-foreground mt-2">{{ $t('user.tip.queryCountTip') }}</p>
-          </CardContent>
-        </Card>
+        <DataCapCard :loading="loading">
+          <template #title>{{ $t('user.common.sourceCount') }}</template>
+          <template #content>
+            <div class="p-5">
+              <div class="text-2xl font-bold">{{ summary.sourceCount }}</div>
+              <p class="text-xs text-muted-foreground mt-2">{{ $t('user.tip.sourceCountTip') }}</p>
+            </div>
+          </template>
+        </DataCapCard>
+        <DataCapCard :loading="loading">
+          <template #title>{{ $t('user.common.queryCount') }}</template>
+          <template #content>
+            <div class="p-5">
+              <div class="text-2xl font-bold">{{ summary.queryCount }}</div>
+              <p class="text-xs text-muted-foreground mt-2">{{ $t('user.tip.queryCountTip') }}</p>
+            </div>
+          </template>
+        </DataCapCard>
       </div>
     </div>
   </div>
@@ -32,10 +31,12 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Loader2 } from 'lucide-vue-next'
 import { HttpUtils } from '@/utils/http'
 import UserService from '@/services/user'
+import { DataCapCard } from '@/views/ui/card'
 
 export default defineComponent({
   name: 'DashboardHome',
   components: {
+    DataCapCard,
     Loader2,
     CardContent, CardTitle, CardHeader, Card
   },
--- core/datacap-ui/src/views/ui/card/DataCapCard.vue
+++ core/datacap-ui/src/views/ui/card/DataCapCard.vue
@@ -1,25 +1,30 @@
 <template>
-  <Card :class="cn('rounded-sm', computedShadow)">
-    <CardHeader v-if="$slots.title || title" :class="cn('flex flex-row items-center justify-between border-b p-3')">
-      <div class="grid gap-2">
-        <CardTitle>
-          <span v-if="title">{{ title }}</span>
-          <slot v-else name="title"/>
-        </CardTitle>
-      </div>
-      <div v-if="$slots.extra">
-        <slot name="extra"/>
-      </div>
-    </CardHeader>
-    <CardContent v-if="$slots.content" class="p-0">
-      <slot name="content"/>
-    </CardContent>
-    <CardContent v-else class="p-0">
-      <slot/>
-    </CardContent>
-    <CardFooter v-if="$slots.footer" class="border-t p-4">
-      <slot name="footer"/>
-    </CardFooter>
+  <Card :class="cn('relative rounded-sm min-h-32', computedShadow)">
+    <div v-if="loading" class="absolute inset-0 bg-gray-100 bg-opacity-50 flex items-center justify-center z-10">
+      <Loader2 class="mr-2 animate-spin text-center text-blue-300"/>
+    </div>
+    <div v-else>
+      <CardHeader v-if="$slots.title || title" :class="cn('flex flex-row items-center justify-between border-b p-3')">
+        <div class="grid gap-2">
+          <CardTitle>
+            <span v-if="title">{{ title }}</span>
+            <slot v-else name="title"/>
+          </CardTitle>
+        </div>
+        <div v-if="$slots.extra">
+          <slot name="extra"/>
+        </div>
+      </CardHeader>
+      <CardContent v-if="$slots.content" class="p-0">
+        <slot name="content"/>
+      </CardContent>
+      <CardContent v-else class="p-0">
+        <slot/>
+      </CardContent>
+      <CardFooter v-if="$slots.footer" class="border-t p-4">
+        <slot name="footer"/>
+      </CardFooter>
+    </div>
   </Card>
 </template>
 
@@ -28,10 +33,12 @@ import { ref, watchEffect } from 'vue'
 import { cn } from '@/lib/utils'
 import { Shadow } from '@/enums/shadow.ts'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Loader2 } from 'lucide-vue-next'
 
 const props = defineProps<{
   title?: string
   shadow?: keyof typeof Shadow
+  loading?: boolean
 }>()
 
 const computedShadow = ref<string>('never')
