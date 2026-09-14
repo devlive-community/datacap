bf8d7147d385a400ef759e7ddc1e0d7eec2c0b78
qianmoQ <shicheng@devlive.org>
2026-09-13 23:33:00 -0400

    refactor(ui): migrate user security pages to ant-design-vue + composition
    Migrate admin/user/security change-password and change-username pages:
    a-card (bordered=false + description line), a-form with a-input /
    a-input-password, form-item :extra for help text, submit via html-type.
    Convert all four files (PasswordHome/PasswordForm, UsernameHome/UsernameForm)
    to <script setup> with useI18n / antd message; validators use antd signature.

--- core/datacap-ui/src/views/pages/admin/user/security/UsernameHome.vue
+++ core/datacap-ui/src/views/pages/admin/user/security/UsernameHome.vue
@@ -1,20 +1,15 @@
 <template>
-  <ShadcnCard :border="false"
-              :title="$t('user.common.modifyUsername')"
-              :description="$t('user.tip.modifyUsername')">
-    <ShadcnDivider class="my-2"/>
+  <a-card :bordered="false" :title="$t('user.common.modifyUsername')">
+    <div class="text-sm text-gray-500">{{ $t('user.tip.modifyUsername') }}</div>
+    <a-divider class="my-2"/>
     <div class="p-3">
       <UsernameForm/>
     </div>
-  </ShadcnCard>
+  </a-card>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import UsernameForm from '@/views/pages/admin/user/security/components/UsernameForm.vue'
 
-export default defineComponent({
-  name: 'UsernameHome',
-  components: { UsernameForm }
-})
+defineOptions({ name: 'UsernameHome' })
 </script>
