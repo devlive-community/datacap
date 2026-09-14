51ecf2cb1a6328868d09e693ccbe5099004fa822
qianmoQ <shicheng@devlive.org>
2026-09-14 02:11:35 -0400

    refactor(ui): migrate source table structure view to ant-design-vue

--- core/datacap-ui/src/views/pages/admin/source/SourceTableStructure.vue
+++ core/datacap-ui/src/views/pages/admin/source/SourceTableStructure.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="relative min-h-screen">
-    <ShadcnSkeleton v-if="loading" animation/>
+    <a-skeleton v-if="loading" active/>
 
-    <ShadcnTable v-else-if="data && data.length > 0"
-                 size="small"
-                 :columns="headers"
-                 :data="data">
-      <template #object_nullable="{ row }">
-        <ShadcnSwitch v-model="row.object_nullable" size="small" disabled/>
+    <a-table v-else-if="data && data.length > 0"
+             size="small"
+             :columns="headers"
+             :data-source="data"
+             :pagination="false"
+             row-key="object_name"
+             :scroll="{ x: 'max-content' }">
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'object_nullable'">
+          <a-switch v-model:checked="record.object_nullable" size="small" disabled/>
+        </template>
       </template>
-    </ShadcnTable>
+    </a-table>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { getCurrentInstance, onMounted, ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute } from 'vue-router'
+import { message } from 'ant-design-vue'
 import { createHeaders } from '@/views/pages/admin/source/components/TableUtils.ts'
 import MetadataService from '@/services/metadata'
 
 const { t } = useI18n()
 const route = useRoute()
-const { proxy } = getCurrentInstance()!
 
 const headers = createHeaders({ t })
 
@@ -30,7 +35,6 @@ const loading = ref(false)
 const data = ref<Array<any>>([])
 
 const handleInitialize = () => {
-
   const code = route.params.source
   const database = route.params.database
   const table = route.params.table
@@ -38,20 +42,16 @@ const handleInitialize = () => {
   if (code && database && table) {
     loading.value = true
     MetadataService.getColumnsByTable(code as string, database as string, table as string)
-                   .then(response => {
+                   .then((response) => {
                      if (response.status && response.data && response.data.isSuccessful) {
                        data.value = response.data.columns
-                                            .filter(col => col.type_name === 'column')
+                                            .filter((col: any) => col.type_name === 'column')
                      }
                      else {
-                       // @ts-ignore
-                       proxy.$Message.error({
-                         content: response.message,
-                         showIcon: true
-                       })
+                       message.error(response.message)
                      }
                    })
-                   .finally(() => loading.value = false)
+                   .finally(() => (loading.value = false))
   }
 }
 
