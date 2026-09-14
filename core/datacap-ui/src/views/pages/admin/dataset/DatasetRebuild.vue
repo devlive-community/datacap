93ca12bd7133aef59224af811df1fb8e11402a9a
qianmoQ <shicheng@devlive.org>
2026-09-14 01:00:03 -0400

    refactor(ui): migrate dataset action dialogs to ant-design-vue + composition
    Migrate DatasetDelete (a-modal + a-alert list + name match validator),
    DatasetRebuild (a-modal + a-alert + rebuild button), DatasetClear (a-modal +
    a-row/a-col stat cards + danger clear) and DatasetSync (a-modal with dynamic
    override fields: a-input / a-input-number / a-switch / a-input-password). All
    <script setup> with useI18n / antd message.

--- core/datacap-ui/src/views/pages/admin/dataset/DatasetRebuild.vue
+++ core/datacap-ui/src/views/pages/admin/dataset/DatasetRebuild.vue
@@ -1,78 +1,57 @@
 <template>
-  <ShadcnModal v-model="visible" :title="$t('dataset.common.rebuild') + ' [ ' + data?.name + ' ]'" @on-close="onCancel">
-    <ShadcnAlert :title="$t('dataset.tip.rebuildProgress')"/>
+  <a-modal v-model:open="visible" :title="$t('dataset.common.rebuild') + ' [ ' + data?.name + ' ]'">
+    <a-alert :message="$t('dataset.tip.rebuildProgress')"/>
 
     <template #footer>
-      <ShadcnButton :disabled="loading" :loading="loading" @click="onRebuild">
+      <a-button type="primary" :disabled="loading" :loading="loading" @click="onRebuild">
         {{ $t('dataset.common.rebuild') }}
-      </ShadcnButton>
+      </a-button>
     </template>
-  </ShadcnModal>
+  </a-modal>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { message } from 'ant-design-vue'
 import DatasetService from '@/services/dataset'
 import { DatasetModel } from '@/model/dataset'
 
-export default defineComponent({
-  name: 'DatasetRebuild',
-  props: {
-    isVisible: {
-      type: Boolean,
-      default: () => false
-    },
-    data: {
-      type: Object as () => DatasetModel | null
-    }
-  },
-  computed: {
-    visible: {
-      get(): boolean
-      {
-        return this.isVisible
-      },
-      set(value: boolean)
-      {
-        this.$emit('close', value)
-      }
-    }
-  },
-  data()
-  {
-    return {
-      loading: false
-    }
-  },
-  methods: {
-    onRebuild()
-    {
-      if (this.data) {
-        this.loading = true
-        DatasetService.rebuild(this.data.code)
-                      .then((response) => {
-                        if (response.status) {
-                          this.$Message.success({
-                            content: `${ this.$t('dataset.common.rebuild') } [ ${ this.data?.name } ] ${ this.$t('common.successfully') }`,
-                            showIcon: true
-                          })
+defineOptions({ name: 'DatasetRebuild' })
 
-                          this.onCancel()
-                        }
-                        else {
-                          this.$Message.error({
-                            content: response.message,
-                            showIcon: true
-                          })
-                        }
-                      })
-                      .finally(() => this.loading = false)
-      }
-    },
-    onCancel()
-    {
-      this.visible = false
-    }
-  }
+const props = withDefaults(defineProps<{ isVisible?: boolean; data?: DatasetModel | null }>(), {
+  isVisible: false,
+  data: null
 })
+const emit = defineEmits<{ (e: 'close', value: boolean): void }>()
+
+const { t } = useI18n()
+
+const visible = computed({
+  get: () => props.isVisible,
+  set: (value: boolean) => emit('close', value)
+})
+
+const loading = ref(false)
+
+const onCancel = () => {
+  visible.value = false
+}
+
+const onRebuild = () => {
+  if (props.data) {
+    loading.value = true
+    DatasetService.rebuild(props.data.code)
+                  .then((response) => {
+                    if (response.status) {
+                      message.success(`${ t('dataset.common.rebuild') } [ ${ props.data?.name } ] ${ t('common.successfully') }`)
+                      onCancel()
+                    }
+                    else {
+                      message.error(response.message)
+                    }
+                  })
+                  .finally(() => (loading.value = false))
+  }
+}
 </script>
